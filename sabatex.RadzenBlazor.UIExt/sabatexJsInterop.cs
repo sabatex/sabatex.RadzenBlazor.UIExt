50b43a6102c79211ed93c2b6f38a9016c94cc0ad
sabatex <serhiy.lakas@hotmail.com>
2022-11-16 12:06:32 +0200

    Add project files.

---
+++ sabatex.RadzenBlazor.UIExt/sabatexJsInterop.cs
@@ -0,0 +1,37 @@
+using Microsoft.JSInterop;
+
+namespace sabatex.RadzenBlazor.UIExt
+{
+    // This class provides an example of how JavaScript functionality can be wrapped
+    // in a .NET class for easy consumption. The associated JavaScript module is
+    // loaded on demand when first needed.
+    //
+    // This class can be registered as scoped DI service and then injected into Blazor
+    // components for use.
+
+    public class sabatexJsInterop : IAsyncDisposable
+    {
+        private readonly Lazy<Task<IJSObjectReference>> moduleTask;
+
+        public sabatexJsInterop(IJSRuntime jsRuntime)
+        {
+            moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
+                "import", "./_content/sabatex.RadzenBlazor.UIExt/exampleJsInterop.js").AsTask());
+        }
+
+        public async ValueTask<string> Prompt(string message)
+        {
+            var module = await moduleTask.Value;
+            return await module.InvokeAsync<string>("showPrompt", message);
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (moduleTask.IsValueCreated)
+            {
+                var module = await moduleTask.Value;
+                await module.DisposeAsync();
+            }
+        }
+    }
+}
